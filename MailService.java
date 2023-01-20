35297057b0f1dfe1b6df06296c95eecba1725ac4
sergr972 <105642625+sergr972@users.noreply.github.com>
2023-01-20 14:24:49 +0300

    Add files via upload

---
+++ MailService.java
@@ -0,0 +1,9 @@
+package com.Mail;
+/*
+Интерфейс, который задает класс, который может каким-либо образом обработать почтовый объект.
+*/
+public interface MailService {
+    Sendable processMail(Sendable mail);
+}
+
+
---
+++ RealMailService.java
@@ -0,0 +1,15 @@
+package com.Mail;
+
+import com.Mail.MailService;
+
+/*
+Класс, в котором скрыта логика настоящей почты
+*/
+public class RealMailService implements MailService {
+
+    @Override
+    public Sendable processMail(Sendable mail) {
+        // Здесь описан код настоящей системы отправки почты.
+        return mail;
+    }
+}
