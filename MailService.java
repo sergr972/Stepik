c721d136783e89bf9e998805f96e0868cd345ee4
sergr972 <sergr972@gmail.com>
2023-01-21 10:52:57 +0300

    изменение пути

--- MailService.java
+++
@@ -1,9 +0,0 @@
-package com.Mail;
-/*
-Интерфейс, который задает класс, который может каким-либо образом обработать почтовый объект.
-*/
-public interface MailService {
-    Sendable processMail(Sendable mail);
-}
-
-
--- Sendable.java
+++
@@ -1,9 +0,0 @@
-package com.Mail;
-    /*
-Интерфейс: сущность, которую можно отправить по почте.
-У такой сущности можно получить от кого и кому направляется письмо.
-*/
-    public interface Sendable {
-        String getFrom();
-        String getTo();
-    }
